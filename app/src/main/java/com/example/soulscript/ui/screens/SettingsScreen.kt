a098146d3b6454e269413dac4904a9fc70d480d4
Aditya Sood <adityasood314@gmail.com>
2025-08-21 18:47:23 +0530

    Update version

--- app/src/main/java/com/example/soulscript/ui/screens/SettingsScreen.kt
+++ app/src/main/java/com/example/soulscript/ui/screens/SettingsScreen.kt
@@ -276,7 +276,7 @@ fun SettingsScreen(
                     subtitle = "View the project on GitHub",
                     onClick = { uriHandler.openUri("https://github.com/adityasood04/Soul-Script") }
                 )
-                SettingInfoItem(title = "App Version", subtitle = "1.8.0")
+                SettingInfoItem(title = "App Version", subtitle = "1.9.0")
             }
         }
     }
